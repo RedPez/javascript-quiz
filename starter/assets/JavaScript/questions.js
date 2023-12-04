942a67600c2d6b516fd9132772bf96de0a2d5d20
unknown <pjrbusiness@outlook.com>
2023-12-04 12:39:44 +0000

    Created questions file. Added questions and answers in an array

---
+++ starter/assets/JavaScript/questions.js
@@ -0,0 +1,55 @@
+const questions = [
+  "What of these answers shows the correct way to declare a variable in JavaScript?",
+  "Which keyword is used to define a function in JavaScript?",
+  "How do you comment out a single line in JavaScript?",
+  "What does the '=== operator' do in JavaScript?",
+  "What is the purpose of the 'else' statement in an 'if-else' statement?",
+  "How can you check the length of an array named 'myArray'?",
+  "What is the purpose of the 'return' keyword in a function?",
+  "What is the role of the 'document.getElementById()' method?",
+  "Which of the following is a valid way to create an object in JavaScript?",
+  "What is the purpose of the 'addEventListener()' method in JavaScript?",
+];
+
+const answers = [
+  ["let number = 10;", "number = 10;", "x = 10 const;", "int x = 10;"],
+  ["function", "def", "fun", "define"],
+  [
+    "/* This is a comment */",
+    "// This is a comment",
+    "# This is a comment",
+    "/ This is a comment",
+  ],
+  [
+    "Checks for equality without type coercion",
+    "Checks for equality with type coercion",
+    "Assigns a value",
+    "Compares references",
+  ],
+  [
+    "To execute the code inside the 'else' block",
+    "To specify an alternative action if the 'if' condition is false",
+    "To terminate the program",
+    "To print a message to the console",
+  ],
+  ["myArray.length()", "lengthOf(myArray)", "myArray.size", "myArray.length"],
+  [
+    "To stop the execution of a function",
+    "To return a value from a function",
+    "To skip the next statement in the function",
+    "To print a message to the console",
+  ],
+  [
+    "Gets the element with the specified ID from the document",
+    "Sets the ID of an HTML element",
+    "Checks if an element exists",
+    "Creates a new HTML element",
+  ],
+  ["var obj = {}", "Object.create({})", "new Object()", "Both A and B"],
+  [
+    "To handle user interactions like clicks",
+    "To create a new HTML element",
+    "To modify the CSS styles of an element",
+    "To define a new function",
+  ],
+];
