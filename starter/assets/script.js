27ba47f7dc12660a1fc8342a34e533a85448690f
unknown <pjrbusiness@outlook.com>
2023-12-04 12:13:00 +0000

    initial commit

---
+++ starter/assets/script.js
@@ -0,0 +1,10 @@
+//Initialise start of quiz:
+
+let startButton = document.getElementById("start");
+
+function startQuiz() {
+  //start clock/countdown
+  //show first question
+}
+
+startButton.addEventListener("click", startQuiz);
