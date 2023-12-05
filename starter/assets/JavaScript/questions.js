3c3a2698380b4b97ebaff46c14f98e400090c3da
unknown <pjrbusiness@outlook.com>
2023-12-05 22:09:24 +0000

    Changed the questions and answers array to key value pair

--- starter/assets/JavaScript/questions.js
+++ starter/assets/JavaScript/questions.js
@@ -1,55 +1,102 @@
 const questions = [
-  "What of these answers shows the correct way to declare a variable in JavaScript?",
-  "Which keyword is used to define a function in JavaScript?",
-  "How do you comment out a single line in JavaScript?",
-  "What does the '=== operator' do in JavaScript?",
-  "What is the purpose of the 'else' statement in an 'if-else' statement?",
-  "How can you check the length of an array named 'myArray'?",
-  "What is the purpose of the 'return' keyword in a function?",
-  "What is the role of the 'document.getElementById()' method?",
-  "Which of the following is a valid way to create an object in JavaScript?",
-  "What is the purpose of the 'addEventListener()' method in JavaScript?",
-];
-
-const answers = [
-  ["let number = 10;", "number = 10;", "x = 10 const;", "int x = 10;"],
-  ["function", "def", "fun", "define"],
-  [
-    "/* This is a comment */",
-    "// This is a comment",
-    "# This is a comment",
-    "/ This is a comment",
-  ],
-  [
-    "Checks for equality without type coercion",
-    "Checks for equality with type coercion",
-    "Assigns a value",
-    "Compares references",
-  ],
-  [
-    "To execute the code inside the 'else' block",
-    "To specify an alternative action if the 'if' condition is false",
-    "To terminate the program",
-    "To print a message to the console",
-  ],
-  ["myArray.length()", "lengthOf(myArray)", "myArray.size", "myArray.length"],
-  [
-    "To stop the execution of a function",
-    "To return a value from a function",
-    "To skip the next statement in the function",
-    "To print a message to the console",
-  ],
-  [
-    "Gets the element with the specified ID from the document",
-    "Sets the ID of an HTML element",
-    "Checks if an element exists",
-    "Creates a new HTML element",
-  ],
-  ["var obj = {}", "Object.create({})", "new Object()", "Both A and B"],
-  [
-    "To handle user interactions like clicks",
-    "To create a new HTML element",
-    "To modify the CSS styles of an element",
-    "To define a new function",
-  ],
+  {
+    question:
+      "Which of these answers shows the correct way to declare a variable in JavaScript?",
+    answer: [
+      "let number = 10;",
+      "number = 10;",
+      "x = 10 const;",
+      "int x = 10;",
+    ],
+    correctAnswer: "let number = 10;",
+  },
+  {
+    question: "Which keyword is used to define a function in JavaScript?",
+    answer: ["function", "def", "fun", "define"],
+    correctAnswer: "function",
+  },
+  {
+    question: "How do you comment out a single line in JavaScript?",
+    answer: [
+      "/* This is a comment */",
+      "// This is a comment",
+      "# This is a comment",
+      "/ This is a comment",
+    ],
+    correctAnswer: "// This is a comment",
+  },
+  {
+    question: "What does the '=== operator' do in JavaScript?",
+    answer: [
+      "Checks for equality without type coercion",
+      "Checks for equality with type coercion",
+      "Assigns a value",
+      "Compares references",
+    ],
+    correctAnswer: "Checks for equality with type coercion",
+  },
+  {
+    question:
+      "What is the purpose of the 'else' statement in an 'if-else' statement?",
+    answer: [
+      "To execute the code inside the 'else' block",
+      "To specify an alternative action if the 'if' condition is false",
+      "To terminate the program",
+      "To print a message to the console",
+    ],
+    correctAnswer:
+      "To specify an alternative action if the 'if' condition is false",
+  },
+  {
+    question: "How can you check the length of an array named 'myArray'?",
+    answer: [
+      "myArray.length()",
+      "lengthOf(myArray)",
+      "myArray.size",
+      "myArray.length",
+    ],
+    correctAnswer: "myArray.length",
+  },
+  {
+    question: "What is the purpose of the 'return' keyword in a function?",
+    answer: [
+      "To stop the execution of a function",
+      "To return a value from a function",
+      "To skip the next statement in the function",
+      "To print a message to the console",
+    ],
+    correctAnswer: "To return a value from a function",
+  },
+  {
+    question: "What is the role of the 'document.getElementById()' method?",
+    answer: [
+      "Gets the element with the specified ID from the document",
+      "Sets the ID of an HTML element",
+      "Checks if an element exists",
+      "Creates a new HTML element",
+    ],
+    correctAnswer: "Gets the element with the specified ID from the document",
+  },
+  {
+    question:
+      "Which of the following is a valid way to create an object in JavaScript?",
+    answer: [
+      "var obj = {}",
+      "Object.create({})",
+      "new Object()",
+      "Both A and B",
+    ],
+    correctAnswer: "Both A and B",
+  },
+  {
+    question:
+      "What is the purpose of the 'addEventListener()' method in JavaScript?",
+    answer: [
+      "To handle user interactions like clicks",
+      "To create a new HTML element",
+      "To modify the CSS styles of an element",
+      "To define a new function",
+    ],
+    correctAnswer: "To handle user interactions like clicks",
+  },
 ];
