d211798e30a88828b7fa34f719eeffe6e2547727
unknown <pjrbusiness@outlook.com>
2023-12-05 22:29:53 +0000

    Building code to display the initial question on click of start button and to add tally to total to be displayed

--- starter/assets/JavaScript/logic.js
+++ starter/assets/JavaScript/logic.js
@@ -21,10 +21,31 @@ function startQuiz() {
       }
     }, 1000);
   }
-  //show first question
 
+  //function to choose random question
+  function displayRandomQuestion() {
+    let randomQuestion = Math.floor(Math.random() * questions.length);
+    let currentQuestion = questions[randomIndex];
+
+    //function to hide <h1>, <p> and <button> and display the first random question.
+
+    /*display answer buttons 
+    funtion => const answerButton = document...grab button
+    //if statement to check if the correct button/answer was clicked
+    if (button === currentQuestion.correctAnswer) {
+      do ++ AND move to the next question
+    } else {
+      do -- AND move to the next question
+    } */
+
+    //displayRandomQuestion();
+  }
   //start timer when quiz begins
   startTimer();
+
+  //show random question when quiz begins
+  displayRandomQuestion();
 }
 
 startButton.addEventListener("click", startQuiz);
+//add event listener for questions button:
